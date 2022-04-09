54bd4bef572546b1de82152438bd388c1a6e4e36
simanxu <simanxu@tencent.com>
2022-04-09 12:02:13 +0800

    Add robot urdf and mesh

---
+++ simulation/controller_plugin/controller_plugin.h
@@ -0,0 +1,68 @@
+#ifndef SIMULATION_CONTROLLER_PLUGIN_CONTROLLER_PLUGIN_H_
+#define SIMULATION_CONTROLLER_PLUGIN_CONTROLLER_PLUGIN_H_
+
+#include <ros/callback_queue.h>
+#include <ros/publisher.h>
+#include <ros/ros.h>
+#include <ros/subscribe_options.h>
+#include <ros/subscriber.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int64.h>
+#include <gazebo/gazebo.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
+#include <string>
+#include <thread>
+#include <vector>
+#include "sensor_data_types.h"
+
+namespace gazebo {
+
+class CarWheelPlugin : public WorldPlugin {
+ public:
+  // 构造函数与析构函数
+  CarWheelPlugin();
+  ~CarWheelPlugin() = default;
+
+  // 在插入本插件的时候，Gazebo就会调用Load()函数。
+  // 该函数是一个虚函数，我们可以通过重载该函数来对插件进行一些初始化的操作。
+  // 输入参数_model是一个指向与本插件相关联的模型的指针。
+  // 输入参数_sdf则是一个指向本插件SDF元素的指针。
+ public:
+  void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
+  void Reset();
+  void InitModel();
+  void OnUpdateEnd();
+  void LoadImuData();
+  void ModeCallback(const std_msgs::Int64& msg);
+  void CarController();
+
+ private:
+  // Pointer to the update event connection
+  event::ConnectionPtr update_connection_;
+
+ private:
+  int iterations_;
+  physics::WorldPtr world_;
+  physics::ModelPtr model_;
+  physics::JointPtr* joint_;
+  unsigned int joint_count_;
+  unsigned int control_mode_;  // 0-Force, 1-JointPD
+  float* q_des_;
+  float* qd_des_;
+  float* tau_des_;
+  IMUSensorData imu_data_;
+
+ private:
+  ros::NodeHandle node_handle_;
+  ros::CallbackQueue callback_queue_;
+  ros::Subscriber sub_mode_;
+};
+
+// 向Gazebo注册本插件
+GZ_REGISTER_WORLD_PLUGIN(CarWheelPlugin);
+
+}  // namespace gazebo
+
+#endif  // SIMULATION_CONTROLLER_PLUGIN_CONTROLLER_PLUGIN_H_
---
+++ simulation/controller_plugin/readme.md
@@ -0,0 +1,8 @@
+### Gazebo Velocity Motor API & Contact Friction Setting.
+1. https://answers.gazebosim.org//question/22033/return-forcestorques-set-to-joints-via-physicsjointsetparamvel/
+2. http://gazebosim.org/tutorials?tut=set_velocity
+3. https://osrf-distributions.s3.amazonaws.com/gazebo/api/dev/classgazebo_1_1physics_1_1Joint.html#a85f6b25f1d0d6451a84875c18c57535d
+4. https://stackoverflow.com/questions/22729445/gazebosim-2-2-2-getting-joint-forces-getforcetorque-warning-odejoint-cc1105
+5. https://answers.gazebosim.org//question/13781/control-of-closed-kinematic-chain-with-a-plugin/
+6. https://answers.gazebosim.org//question/9371/finger-contact-erp-cfm-kp-and-kd/
+7. http://www.piaoyi.org/linux/Linux-touch-changetime.html
