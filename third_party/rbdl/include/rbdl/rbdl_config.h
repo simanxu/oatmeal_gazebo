1b102e1fba115ab9641e5cecffc23416c10458f4
simanxu <simanxu@tencent.com>
2022-04-09 11:31:40 +0800

    Benchmark: Add third_party and success compile
    添加若干第三方库，并且成功编译

---
+++ third_party/rbdl/include/rbdl/rbdl_config.h
@@ -0,0 +1,15 @@
+// Copyright 2021 Tencent.
+#ifndef RBDL_CONFIG_H_
+#define RBDL_CONFIG_H_
+
+#define RBDL_API_VERSION (3 << 16) + (0 << 8) + 0
+#define RBDL_BUILD_COMMIT "unknown"
+#define RBDL_BUILD_TYPE "unknown"
+#define RBDL_BUILD_BRANCH "unknown"
+
+// Disables these macro for static build.
+#define RBDL_DLLAPI
+#define RBDL_LOCAL
+#define RBDL_ADDON_DLLAPI
+
+#endif  // RBDL_CONFIG_H_
